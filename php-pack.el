7f779064c195a8741be919d554735ff6de37bdf0
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-30 21:55:22 +0200

    Use use-package

--- php-pack.el
+++ php-pack.el
@@ -4,17 +4,14 @@
 
 ;;; Code:
 
-(require 'install-packages-pack)
-(install-packages-pack/install-packs '(php-mode
-                                       smartscan))
-
-(add-to-list 'auto-mode-alist '("\\.module$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.install$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.engine$" . php-mode))
-
-(require 'smartscan)
-(add-hook 'php-mode-hook (lambda () (smartscan-mode)))
+(use-package smartscan)
+(use-package php-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.module$" . php-mode))
+  (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+  (add-to-list 'auto-mode-alist '("\\.install$" . php-mode))
+  (add-to-list 'auto-mode-alist '("\\.engine$" . php-mode))
+  (add-hook 'php-mode-hook 'smartscan-mode))
 
 (provide 'php-pack)
 ;;; php-pack.el ends here
